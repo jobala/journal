86f9e333635253c7296e331e29191f24eeffda24
jobala <japhethobalak@gmail.com>
2021-02-04 12:56:30 +0300

    Load HTML

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { app, BrowserWindow } from 'electron';
 let win: BrowserWindow;
 
 app.on('ready', () => {
-  win = new BrowserWindow();
+  win = new BrowserWindow({
+    height: 700,
+    width: 1200,
+  });
+
+  win.loadURL(`file://${__dirname}/../src/main.html`);
   win.webContents.openDevTools();
 });
